06f89c656ad4cd21caaf80b0971a5e4906c2e9cd
linuxmercedes <linuxmercedes@gmail.com>
2019-01-19 12:48:48 -0600

    README and pip script

---
+++ README.rst
@@ -0,0 +1,13 @@
+Redkyn-common |pypi| |travis|
+=============================
+
+.. |pypi| image:: https://badge.fury.io/py/redkyn-common.svg
+    :target: https://badge.fury.io/py/redkyn-common
+
+.. |travis| image:: https://travis-ci.org/redkyn/redkyn-common.svg?branch=master
+    :target: https://travis-ci.org/redkyn/redkyn-common
+
+Common libraries for `Assigner <https://github.com/redkyn/assigner>`_ and `Grader <https://github.com/redkyn/grader`_.
+
+Libraries
+---------
---
+++ setup.py
@@ -0,0 +1,77 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='redkyn-common',
+
+    # Derive version from git tags
+    use_scm_version=True,
+
+    description='Common libraries for Redkyn projects',
+    long_description=long_description,
+
+    url='https://github.com/redkyn/redkyn-common',
+
+    author='Natasha Jarus and Islam Elnabarawi',
+
+    author_email='jarus@mst.edu',
+
+    # For a list of valid classifiers, see
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Education',
+        'Topic :: Education',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+
+    # This field adds keywords for your project which will appear on the
+    # project page. What does your project relate to?
+    #keywords='sample setuptools development',
+
+    packages=find_packages(),
+
+    setup_requires=['setuptools_scm>=1.15'],
+
+    # This field lists other packages that your project depends on to run.
+    # Any package you put here will be installed by pip when your project is
+    # installed, so they must be valid existing projects.
+    install_requires=[
+        'requests>=2.20.0',
+    ],
+
+    python_requires='>=3.4',
+)
